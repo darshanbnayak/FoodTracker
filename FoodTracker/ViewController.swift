0d0459f2c82ebe838220b0fb710172f70d118fe8
darshanbnayak <darshanbnayak@gmail.com>
2017-01-30 16:32:22 -0800

    Done till adding the image. Tested. OK.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    
+    //MARK: Properties
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!  //Outlet for Textbox
+    @IBOutlet weak var mealNameLabel: UILabel!  //Outlet for Label.
+    //Outlets are a way of setting/getting values from the objects on the canvas.
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad() //predefined function, which basically invokes the viewDidLoad() of parent class.
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self   //sets the delegate of text field as this class (ViewController class).
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()    //here, we are resigning from the role of the textfield being the first responder.
+        return true
     }
-
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //basic need of this method is to be able to read what the user typed in the keypad
+        mealNameLabel.text = textField.text //We just assigned text typed by the user in the textfield to the label!!
+    }
+    
+    //MARK: Actions
+    
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
